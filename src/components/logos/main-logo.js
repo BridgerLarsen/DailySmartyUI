728cf6b28d4c488a1d811bf1298ed085c409d8ee
Bridger Larsen <bridgerjimmy@gmail.com>
2021-02-15 19:23:01 -0700

    setup logo component/styles and changed page title

---
+++ src/components/logos/main-logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import LogoImg from '../../../static/assets/ds-circle-logo.png';
+
+const Logo = () => {
+    return (
+        <div className="logo-main">
+            <img src={LogoImg} />
+        </div>
+    )
+}
+
+export default Logo;
